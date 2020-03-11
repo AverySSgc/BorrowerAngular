adecaebd9341ea808255f483e7c725d79f3d98fa
AverySSgc <avery.corbett@smoothstack.com>
2020-03-11 16:03:09 -0400

    borrower folder moved

---
+++ src/app/services/borrower.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BorrowerService {
+
+  constructor(private http: HttpClient) { }
+
+  borrower = {
+    ".id": null,
+    "name": null,
+    "phone": null,
+    "address": null
+  }
+
+  establishBorrower(inputId) {
+    return this.http.get(`http://localhost:3000/borrowers/${inputId}`);
+  }
+
+  getBorrower() {
+    return this.borrower;
+  }
+
+  setBorrower(newBorrower) {
+    this.borrower = newBorrower;
+  }
+}
